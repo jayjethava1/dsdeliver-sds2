62111e0b94c312b1374152ca455c9ccb640ca355
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2021-01-09 19:59:20 -0300

    fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -10,30 +10,33 @@ import com.devsuperior.dsdeliver.entities.Order;
 import com.devsuperior.dsdeliver.entities.OrderStatus;
 
 public class OrderDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String address;
-	private Double latitude;	
+	private Double latitude;
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
-	
+	private Double total;
+
 	private List<ProductDTO> products = new ArrayList<>();
-	
+
 	public OrderDTO() {
 	}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
+			Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
-	
+
 	public OrderDTO(Order entity) {
 		id = entity.getId();
 		address = entity.getAddress();
@@ -41,8 +44,8 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
-		products = entity.getProducts().stream()
-				.map(x -> new ProductDTO(x)).collect(Collectors.toList());
+		total = entity.getTotal();
+		products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
 
 	public Long getId() {
@@ -91,6 +94,14 @@ public class OrderDTO implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public List<ProductDTO> getProducts() {
